716c9c1caa46502b77374d30df54c0e7c2682678
itsatifsiddiqui <itsatifsiddiqui@gmail.com>
2022-01-13 21:43:56 +0500

    show dialogue

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,10 +1,59 @@
-import { CircularProgress, Grid } from '@mui/material';
+import { CircularProgress, Grid, Container } from '@mui/material';
 import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+
+function SimpleDialog(props, account) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>{selectedValue.name} Account</DialogTitle>
+      <DialogTitle>{selectedValue.fullLink}</DialogTitle>
+      <Container sx={{ minHeight: 12 }} />
+    </Dialog>
+  );
+}
+
+SimpleDialog.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
+  selectedValue: PropTypes.object.isRequired,
+};
 
 function ImageItem({ account, width }) {
 
   const [isImageLoading, setIsImageLoading] = useState(true);
 
+  function isValidHttpUrl(string) {
+    let url;
+
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(account);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
 
 
   return (
@@ -12,18 +61,29 @@ function ImageItem({ account, width }) {
 
 
       {isImageLoading && <CircularProgress color="success" style={{ marginTop: 16 }} />}
+      <>
+        <img
+          src={account.image}
+          alt='account images'
+          width={width}
+          onClick={() => {
+            if (isValidHttpUrl(account.fullLink)) {
+              window.open(account.fullLink);
+            }
+            handleClickOpen();
+          }}
+          onLoad={() => {
+            setIsImageLoading(false);
+          }}
+        />
+        <SimpleDialog
+          selectedValue={selectedValue}
+          open={open}
+          onClose={handleClose}
+          account={account}
+        />
+      </>
 
-      <img
-        src={account.image}
-        alt='account images'
-        width={width}
-        onClick={() => {
-          window.open(account.fullLink);
-        }}
-        onLoad={() => {
-          setIsImageLoading(false);
-        }}
-      />
     </Grid>
   )
 }
